8d76f002b601784bb1de4a9b80f35a5bc8d0ef88
kvdpxne <kvdpxne@gmail.com>
2024-08-23 01:50:31 +0200

    JitPack setup.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,25 @@
 plugins {
   id("java")
+  id("maven-publish")
 }
 
 group = "me.kvdpxne"
-version = "1.0-SNAPSHOT"
+version = "0.1"
 
-repositories {
-  mavenCentral()
-  mavenLocal()
-
-  maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-}
 dependencies {
   implementation("org.spigotmc:spigot-api:1.20.6-R0.1-SNAPSHOT")
+}
+
+publishing {
+  publications {
+    register("mavenJava", MavenPublication::class) {
+      groupId = "me.kvdpxne"
+      artifactId = "notchity"
+      version = "0.1.0"
+
+      from(components["java"])
+    }
+  }
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,22 @@
+pluginManagement {
+
+  repositories {
+    gradlePluginPortal()
+
+    mavenCentral()
+    mavenLocal()
+  }
+}
+
+dependencyResolutionManagement {
+
+  repositories {
+    mavenCentral()
+    mavenLocal()
+
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
+  }
+}
+
 rootProject.name = "notchity"
 
